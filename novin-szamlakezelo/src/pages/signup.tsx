abb852a2733326063c6154d5a03ad85a32a08c4c
AnGoalNa <nandor0601@gmail.com>
2023-12-21 00:42:29 +0100

    started signup page frontend

--- novin-szamlakezelo/src/pages/signup.tsx
+++ novin-szamlakezelo/src/pages/signup.tsx
@@ -10,17 +10,39 @@ import Router from 'next/router';
 import { useState, type FormEvent } from 'react';
 import { useMutateUser, useCreateUser } from '../lib/hooks';
 
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "../components/ui/card"
+
+import { Input } from "../components/ui/input"
+import { Button } from "../components/ui/button"
+import { Label } from "../components/ui/label"
+import {
+    Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+  } from "../components/ui/select"
+
 const Signup: NextPage = () => {
     const [name, setName] = useState('')
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [roles, setRoles] = useState(['user'] as any);
+    const [roles, setRoles] = useState({});
     const { trigger: signup } = useCreateUser();
 
     async function onSignup(e: FormEvent) {
         e.preventDefault();
         try {
-            await signup({ data: { name, username, password, roles: {connect: [{id:'user'}]} } });
+            await signup({ data: { name, username, password, roles: roles } });
         } catch (err: any) {
             console.error(err);
             if (err.info?.prisma && err.info?.code === 'P2002') {
@@ -36,6 +58,48 @@ const Signup: NextPage = () => {
         await signIn('credentials', { redirect: false, username, password });
         await Router.push('/');
     }
+
+    return(
+        <main className='w-full min-h-screen flex items-center justify-center'>
+
+        <Card className='max-w-md w-full'>
+  <CardHeader>
+    <CardTitle>Regisztráció</CardTitle>
+  </CardHeader>
+  <CardContent className='flex flex-col gap-4'>
+    <div className="grid w-full max-w-sm items-center gap-1.5">
+      <Label htmlFor="name">Teljes név</Label>
+      <Input type="text" id="name" placeholder="" value={name} onChange={(e) => setName(e.currentTarget.value)} />
+    </div>
+    <div className="grid w-full max-w-sm items-center gap-1.5">
+      <Label htmlFor="username">Felhasználónév</Label>
+      <Input type="text" id="username" placeholder="" value={username} onChange={(e) => setUsername(e.currentTarget.value)} />
+    </div>
+    <div className='flex w-full items-end gap-4'>
+    <div className="grid w-full max-w-sm items-center gap-1.5">
+      <Label htmlFor="name">Jelszó</Label>
+      <Input type="password" id="password" placeholder="" value={password} onChange={(e) => setPassword(e.currentTarget.value)} />
+    </div>
+    <Select onValueChange={(e) => setRoles({connect: [{id: e}]})}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Szerepkör" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Szerepkörök</SelectLabel>
+          <SelectItem value="user">Felhasználó</SelectItem>
+          <SelectItem value="accountant">Könyvelő</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+    </div>
+  </CardContent>
+  <CardFooter>
+    <p>Card Footer</p>
+  </CardFooter>
+</Card>
+        </main>
+    )
 
     return (
         <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
