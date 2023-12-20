cada5f7f428d2e3d8da4829309d6c6915683de2b
AnGoalNa <nandor0601@gmail.com>
2023-12-20 19:41:24 +0100

    init nextjs with prisma, tailwind and nextauth

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# novin-projekt
+# novin
+
--- novin/app/szamla/list.tsx
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@/utils/supabase/server';
-import { cookies } from 'next/headers';
-
-export default async function Szamlak() {
-  const cookieStore = cookies()
-  const supabase = createClient(cookieStore);
-
-  return 1
-}
